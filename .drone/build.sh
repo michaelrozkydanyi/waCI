b25441d25f69b64a8beaa60d206a1794d7d7c1cb
michael.rozkydanyi <michael.rozkydanyi@gmail.com>
2016-04-29 19:32:16 +0300

    29.wa.html

--- .drone/build.sh
+++ .drone/build.sh
@@ -12,7 +12,8 @@ cp /etc/hosts ~/hosts.new
 sed -i '2 i192.168.33.22 registry.local.lab registry' ~/hosts.new
 cp -f ~/hosts.new /etc/hosts
 
-docker build -t registry.local.lab:5000/academy:1 .
+docker build -t academy:1 .
 #docker commit edipro-deb79 registry.edi.su/edipro-deb79
 docker login -u user -p user registry.local.lab
-docker push registry.local.lab:5000/academy:1
+docker tag academy:1 registry.local.lab/academy:1
+docker push registry.local.lab/academy:1
