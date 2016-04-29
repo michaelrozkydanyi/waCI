b515c6311c27aa18f2f7ffbc20440e0acc931641
michael.rozkydanyi <michael.rozkydanyi@gmail.com>
2016-04-29 18:12:35 +0300

    29.bulid.sh

--- .drone/build.sh
+++ .drone/build.sh
@@ -8,7 +8,8 @@ set -e
 wrapdocker &  
 sleep 5
 
+sed -i '2 i192.168.33.22 registry.local.lab registry' /etc/hosts
+
 docker build -t registry.local.lab:5000/academy:1 .
 #docker commit edipro-deb79 registry.edi.su/edipro-deb79
 docker push registry.local.lab:5000/academy:1
-
