b24f2207c12d7c5151c41c6a8921c3aa8733e66f
michael.rozkydanyi <michael.rozkydanyi@gmail.com>
2016-04-29 19:49:04 +0300

    29.wa.html

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -25,9 +25,9 @@ sleep 10
 #curl -X POST -H 'X-Access-Token:user:$2a$10$Pp6bdgNtf0e.9QooaQ5YbOiD0IZmpkBxWCtfR64lQpj9K.a38ViG2' -H "Content-Type:application/json" -d '{"Name":"ocker", "Running":true,"Paused":false,"Restarting":false, "Image": "docker.lab.int:5000/academy:1", "AttachStdin":false, "AttachStdout":false, "Tty":false, "OpenStdin":false, "DisableNetwork":false, "OOMKilled":false,"Dead":false, "Args":["-g","daemon off;"], "Cmd":["nginx","-g","daemon off;"], "PublishAllPorts":true, "RestartPolicy": {"Name": "always"}}' http://10.0.0.132:8080/containers/academy_1/start
 
 
- ./.drone/rancher-compose --url http://rancher-b.lab.int:8080 --access-key 156261975E6F519F56CC --secret-key bJMweuM5ocnfn9Ypd4vwjSAUQMiJBDdG9eWmHm9P -p mytest rm --force
+ ./.drone/rancher-compose --url http://http://192.168.33.1:8080 --access-key F0162E737817400F804D --secret-key rj3uCau2s1ddPwue7HbwukCCjD8wXkjfr1amTu9H -p mytest rm --force
 
 sleep 40
- ./.drone/rancher-compose --url http://rancher-b.lab.int:8080 --access-key 156261975E6F519F56CC --secret-key bJMweuM5ocnfn9Ypd4vwjSAUQMiJBDdG9eWmHm9P -p mytest up -d
+ ./.drone/rancher-compose --url http://http://192.168.33.1:8080 --access-key F0162E737817400F804D --secret-key rj3uCau2s1ddPwue7HbwukCCjD8wXkjfr1amTu9H -p mytest up -d
 
 # ./.drone/rancher-compose --url http://rancher-b.lab.int:8080 --access-key 156261975E6F519F56CC --secret-key bJMweuM5ocnfn9Ypd4vwjSAUQMiJBDdG9eWmHm9P -p mytest restart
